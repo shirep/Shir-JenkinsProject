cb9c8b97f56ab9d759a74043e7f6a58585ac4021
shirep <140007386+shirep@users.noreply.github.com>
2023-12-01 17:01:28 +0200

    Update app..py

--- Jenkins/python/app..py
+++ Jenkins/python/app..py
@@ -1,14 +1,23 @@
 import boto3
+import os
 import time
 from pythonjsonlogger import jsonlogger
-import os
+import logging
 
-logger = JSONLogger()
+# Configure JSON logger
+logger = logging.getLogger()
+logHandler = logging.StreamHandler()
+formatter = jsonlogger.JsonFormatter()
+logHandler.setFormatter(formatter)
+logger.addHandler(logHandler)
+logger.setLevel(logging.INFO)
 
 def get_instances():
-    ec2 = boto3.client('ec2')
+    access_key = os.environ.get('AWS_ACCESS_KEY_ID')
+    secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
-    # Use the client to get information about instances
+    ec2 = boto3.client('ec2', region_name='eu-north-1', aws_access_key_id=access_key, aws_secret_access_key=secret_key)
+
     response = ec2.describe_instances(
         Filters=[
             {
@@ -22,37 +31,29 @@ def get_instances():
         ]
     )
 
-    # Extract the instance information from the response
     instances = []
     for reservation in response['Reservations']:
         for instance in reservation['Instances']:
             instances.append(instance)
 
-    # Create a list to store instance details
-    instances_info = []
-
-    # Extract the instance IDs and instance names
-    for instance in instances:
-        instance_id = instance['InstanceId']
-        instance_name = next((tag['Value']
-                              for tag in instance['Tags'] if tag['Key'] == 'Name'), 'Unnamed')
-        instances_info.append({"Instance ID": instance_id, "Instance Name": instance_name})
-
-    return instances_info
+    return instances
 
 def main():
-    # קריאה למשתנה סביבתי שמגדיר את הזמן בין רצות הקוד (בשניות)
-    interval_seconds = int(os.getenv("RUN_INTERVAL_SECONDS", 300))
-
     while True:
-        # Get instances information
-        instances_info = get_instances()
+        instances = get_instances()
+
+        instances_info = []
+        for instance in instances:
+            instance_id = instance['InstanceId']
+            instance_name = next((tag['Value']
+                                  for tag in instance['Tags'] if tag['Key'] == 'Name'), 'Unnamed')
+            instances_info.append({"Instance ID": instance_id, "Instance Name": instance_name})
 
         # Log the instances information using json-logger
         logger.info("Instance information", extra={"instances": instances_info})
 
-        # Wait for the specified interval
-        time.sleep(interval_seconds)
+        # Wait for the specified interval (5 minutes)
+        time.sleep(300)
 
 if __name__ == "__main__":
     main()
